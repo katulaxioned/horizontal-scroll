cd69fc2b874a1d8d4b2db19a7353180fde88fe84
atul <atulk@axioned.com>
2022-08-02 19:52:26 +0530

    Half animation added.

--- main.js
+++ main.js
@@ -1,34 +1,78 @@
-const props = {
+const startAnimation = {
   img1: {
-    x: 144,
-    y: 35.239,
-    r: -16.0763
-  }
-}
+    rotation: 40 * (Math.random() - 0.5),
+    y: 100 * (Math.random() - 0.5) + "px",
+    x: 0.1 * window.innerWidth + "px",
+  },
+};
 
-let img1 = $(".two li:nth-child(1) img");
-let isEnter = true;
+const endAnimation = {
+  img1: {
+    rotation: 40 * (Math.random() - 0.5),
+    y: 100 * (Math.random() - 0.5) + "px",
+    x: -0.1 * window.innerWidth + "px",
+  },
+};
 
-let screenWidth = screen.width;
-const scrollWidth= window.innerWidth-$(document).width()
-screenWidth += scrollWidth;
-screenWidth = 500;
+console.log(endAnimation.img1);
 
-$(document).ready(function() {
-  $(window).scroll(function() {
+gsap.registerPlugin(ScrollTrigger);
 
-    // for right side of screen
-    let imgLeft = img1.offset().left;
-    // let leftSpacerScreenWidth = screenWidth - img1.style.width;
-    leftSpacerScreenWidth = 300;
-    console.log(screenWidth, imgLeft)
+let sections = gsap.utils.toArray(".section");
 
-    if (screenWidth > imgLeft && isEnter) {
-      img1.addClass("og-position");
-    } else if (leftSpacerScreenWidth < imgLeft) {
-      img1.removeClass("og-position");
-      isEnter = false;
-    }
+let scrollTween = gsap.to(sections, {
+  xPercent: -100 * (sections.length - 1),
+  ease: "none",
+  scrollTrigger: {
+    trigger: ".container",
+    pin: true,
+    scrub: 1,
+    end: () => "+=3000",
+  },
+});
 
-  })
-})
+let img1 = document.querySelector(".three ul li:nth-child(1) img");
+
+img1.style.cssText = `transform:translate(144px, ${startAnimation.img1.y}) rotate(${startAnimation.img1.rotation}deg);`;
+// img1.style.cssText = `transform:translate(0, 0) rotate(0);`;
+
+gsap.to(img1, {
+  scrollTrigger: {
+    trigger: img1,
+    containerAnimation: scrollTween,
+    start: "left right",
+    end: () =>
+      "+=" +
+      document.querySelector(".three ul li:nth-child(1) img").offsetWidth,
+    markers: true,
+    toggleActions: "play none none reverse",
+    scrub: !0,
+  },
+  // x: 144,
+  // y: 100 * (Math.random() - 0.5),
+  // rotation: 40 * (Math.random() - 0.5),
+  x: 0,
+  y: 0,
+  rotation: 0,
+  duration: 1,
+  ease: "power2.out"
+});
+
+gsap.to(img1, {
+  scrollTrigger: {
+    trigger: img1,
+    containerAnimation: scrollTween,
+    start: "left left",
+    end: () =>
+      "+=" + "-" +
+      document.querySelector(".three ul li:nth-child(1) img").offsetWidth,
+    markers: true,
+    toggleActions: "play none none reverse",
+    scrub: 1,
+  },
+  x: -144,
+  y: 100 * (Math.random() - 0.5),
+  rotation: 40 * (Math.random() - 0.5),
+  duration: 1,
+  ease: "power2.out"
+});
